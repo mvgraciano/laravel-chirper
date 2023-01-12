c100ce7eab7a53d7e1838d1aaf81aef57f2f442b
Marcus Graciano <marcusgraciano.ads@outlook.com>
2023-01-12 18:30:42 -0300

    added feature 'Creating Chirps'

--- app/Models/User.php
+++ app/Models/User.php
@@ -20,7 +20,7 @@ class User extends Authenticatable
     protected $fillable = [
         'name',
         'email',
-        'password',
+        'password'
     ];
 
     /**
@@ -41,4 +41,8 @@ class User extends Authenticatable
     protected $casts = [
         'email_verified_at' => 'datetime',
     ];
+
+    public function chirps(){
+        return $this->hasMany(Chirp::class);
+    }
 }
